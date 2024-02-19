625640fa2aa8079a97279006e81c2c6cb7659025
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2024-02-19 15:38:44 -0500

    fix error

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
 
-
-
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -18,8 +16,7 @@ export default function Page() {
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
-            williamn pére 
+            , brought to you by Vercel. williamn pére
           </p>
           <Link
             href="/login"
@@ -32,21 +29,19 @@ export default function Page() {
           {/* Add Hero Images Here */}
 
           <Image
-        src="/hero-desktop.png"
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="Screenshots of the dashboard project showing desktop version"
-      />
-  <Image
-        src="/hero-mobile.png"
-        width={560}
-        height={620}
-        className="block md:hidden"
-        alt="Screenshot of the dashboard project showing mobile version"
-      />
-
-
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshot of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
